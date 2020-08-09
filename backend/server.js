bcc0a1e5576cccb735949f7510adb220a8d66e42
tunde <icepaigez@gmail.com>
2020-08-09 19:33:59 +0100

    started the bet9ja scraper

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,13 @@ require('dotenv').config();
 
 const utils = require("./utils/utils");
 const naijabet = require("./scrapers/naijabet/naijabet").naijabetScraper;
-//const db = require("./database/db")
+const db = require("./database/db")
+
 
 //select all from table where game equals game, filtered by time, return most recent
 //naijabet().then(console.log) 
-//naijabet() 
 
 
 
-//db('naijabet_3way').where('game', 'Malmo FF vs Orebro SK').then(console.log)
+
+//db('naijabet_3way').where('game', 'Denmark vs Belgium').then(console.log)
