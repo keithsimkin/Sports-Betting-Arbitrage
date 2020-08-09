cbe627f7a275be10c86c3dd6c321334e5f291788
tunde <icepaigez@gmail.com>
2020-08-09 19:05:13 +0100

    test run of naijabet scraper

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,10 @@ const utils = require("./utils/utils");
 const naijabet = require("./scrapers/naijabet/naijabet").naijabetScraper;
 //const db = require("./database/db")
 
-//naijabet()   
+//select all from table where game equals game, filtered by time, return most recent
+//naijabet().then(console.log) 
+//naijabet() 
+
+
+
+//db('naijabet_3way').where('game', 'Malmo FF vs Orebro SK').then(console.log)
