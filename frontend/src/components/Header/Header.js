1e12b1251223377d1464e97acc8fe22a829d1e46
tunde <icepaigez@gmail.com>
2020-09-14 12:11:44 +0100

    set up base components

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div>
+			{
+				// <SearchBar />
+				// <UserProfile />
+					}
+		</div>
+	)
+}
+
+export default Header;
---
+++ frontend/src/components/Header/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBar = () => {
+	return (
+		<div>
+			<input type="text" name="" placeholder="Search arbs for specific games"/>
+		</div>
+	)
+}
+
+export default SearchBar;
