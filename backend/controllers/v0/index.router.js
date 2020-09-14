dd2d716ad463a32a461e2ca33b99718e3430fa82
tunde <icepaigez@gmail.com>
2020-09-14 07:43:55 +0100

    created an endpoint for the root homepage

--- backend/controllers/v0/index.router.js
+++ backend/controllers/v0/index.router.js
@@ -1,11 +1,13 @@
-"use strict";
+ "use strict";
 
 const express = require("express");
 const router = express.Router();
 
 const { ArbsRouter } = require("./arbs/routes/arbs.router");
+const { RootRouter } = require("./root/routes/root.router");
 
 router.use('/arbs', ArbsRouter);
+router.use('/', RootRouter);
 
 router.get('/', (req, res) => { 
 	res.send('v0');
---
+++ backend/controllers/v0/root/routes/root.router.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const db = require("../../../../knex");
+
+
+
+router.get('/', (req, res) => {
+	res.status(200).send('this is the path to the root home page')
+}); 
+
+
+
+exports.RootRouter = router;
