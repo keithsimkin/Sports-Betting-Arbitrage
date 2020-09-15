cadf570fcd4e98b80152c2d795df26f9e600f42a
tunde <icepaigez@gmail.com>
2020-09-15 09:03:29 +0100

    changed the classname of the base app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from "./components/Header/Header";
+//import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
         
       {
         // <Header />
