f671b023e57c74885dc5903b432e54de3e023e8d
tunde <icepaigez@gmail.com>
2020-09-16 22:13:32 +0100

    styling the card component

--- frontend/src/components/CardHolder/Card/Card.js
+++ frontend/src/components/CardHolder/Card/Card.js
@@ -19,14 +19,28 @@ const Card = ({ id, game, game_date, game_time, arb_combination, arb_received })
 	const gamesArray = extractGames("Ts Galaxy - Steenberg Utd")
 	return(
 		<div className="card">
-			This is a card
-			This is a card
-			This is a card
-			This is a card
-			This is a card
-			This is a card
-			This is a card
-			This is a card
+			<table>
+				<thead>
+					<tr>
+						<th>Profit</th>
+						<th>Game</th>
+						<th>Market</th>
+						<th>Bet1</th>
+						<th>Bet2</th>
+						<th>Bet3</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>0.6%</td>
+						<td>Stoke City vs Bristol City</td>
+						<td>1 X 2</td>
+						<td>Bet9ja_Home: 1.84</td>
+						<td>Bet9ja_Draw: 3.58</td>
+						<td>Naijabet_Away: 5.81</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	)
 };
