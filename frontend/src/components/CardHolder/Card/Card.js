f152b63d27f6eb483e5379197648d437fb03f02f
tunde <icepaigez@gmail.com>
2020-09-17 18:08:33 +0100

    styling the card component

--- frontend/src/components/CardHolder/Card/Card.js
+++ frontend/src/components/CardHolder/Card/Card.js
@@ -9,7 +9,7 @@ const extractGames = (str) => {
 	} else if (str.includes(" vs ")) {
 		strArr = str.split(" vs ")
 		return strArr;
-	} else if (str.includes(" v ")){
+	} else if (str.includes(" v ")){ 
 		strArr = str.split(" v ")
 		return strArr; 
 	}
@@ -19,18 +19,7 @@ const Card = ({ id, game, game_date, game_time, arb_combination, arb_received })
 	const gamesArray = extractGames("Ts Galaxy - Steenberg Utd")
 	return(
 		<div className="card">
-			<table>
-				<thead>
-					<tr>
-						<th>Arb</th>
-						<th>Game</th>
-						<th>Market</th>
-						<th>Bet1</th>
-						<th>Bet2</th>
-						<th>Bet3</th>				
-					</tr>
-				</thead>
-			</table>
+			
 		</div>
 	)
 };
--- frontend/src/components/CardHolder/CardHolder.js
+++ frontend/src/components/CardHolder/CardHolder.js
@@ -5,6 +5,19 @@ import "./cardholder.css";
 const CardHolder = ({ arbGames }) => {
 	return(
 		<section className="cardholder">
+			<table>
+				<thead>
+					<tr>
+						<th>Arb</th>
+						<th>Game</th>
+						<th>Market</th>
+						<th>Bet1</th>
+						<th>Bet2</th>
+						<th>Bet3</th>				
+					</tr>
+				</thead>
+			</table>
+			<div id="arbs_list_separator"></div>
 			<Card />
 		</section>
 	)
