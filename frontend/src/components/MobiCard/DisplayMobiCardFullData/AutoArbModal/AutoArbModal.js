88467f837901f79c3451e7d678fe60cba830d050
tunde <icepaigez@gmail.com>
2020-10-07 19:07:17 +0100

    fixed the auto-arbs form

--- frontend/src/components/MobiCard/DisplayMobiCardFullData/AutoArbModal/AutoArbModal.js
+++ frontend/src/components/MobiCard/DisplayMobiCardFullData/AutoArbModal/AutoArbModal.js
@@ -5,8 +5,18 @@ import "./autoarbmodal.css";
 
 class AutoArbModal extends Component {
 
-	autoArb = () => {
-		console.log("i need a puppet")
+	constructor(){
+		super()
+		this.state = {
+			odds1: "",
+			odds2:"",
+			odds3:""
+		}
+	}
+
+	inputChange = e => {
+		let stakeAmount = Number(e.target.value) 
+		console.log(stakeAmount)
 	}
 
 	render() {
@@ -26,10 +36,43 @@ class AutoArbModal extends Component {
 								<p className="modal-title">6.76% | FC Tver - Chita</p>
 							</div>
 							<div className="modal-body">
-								Lorem ipsum
+								<div className="arb-form">
+									<div className="row">
+										<div className="form-data">
+											<label htmlFor="stake" className="label">Stake Amount </label> 
+										</div>
+										<div className="form-input">
+											<input onChange={this.inputChange} type="text" name="stake" id="stake" placeholder="Your stake..." maxLength="10" size="5"/>
+										</div>
+									</div>
+									<div className="row">
+										<div className="form-data">
+											<label htmlFor="odds1" className="label">Bet9ja @ 2.23</label>
+										</div>
+										<div className="form-input">
+											<input type="text" name="odds1" id="odds1" maxLength="10" size="5"/>
+										</div>
+									</div>
+									<div className="row">
+										<div className="form-data">
+											<label htmlFor="odds2" className="label">Naijabet @ 2.40</label>
+										</div>
+										<div className="form-input">
+											<input type="text" name="odds2" id="odds2" maxLength="10" size="5"/>
+										</div>
+									</div>
+									<div className="row">
+										<div className="form-data">
+											<label htmlFor="odds3" className="label">18Bet @ 5.93</label>
+										</div>
+										<div className="form-input">
+											<input type="text" name="odds3" id="odds3" maxLength="10" size="5"/>
+										</div>
+									</div>
+								</div>
 							</div>
 							<div className="modal-footer">
-								<button className="modal-button arb" onClick={this.autoArb}>Arb</button>
+								<button className="modal-button arb">Arb</button>
 								<button className="modal-button close" onClick={closeAutoArbModal}>Close</button>
 							</div>
 						</div>
