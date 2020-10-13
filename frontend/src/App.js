12a88263aa8a97d09446d2e743b85892c84ab078
tunde <icepaigez@gmail.com>
2020-10-13 08:07:56 +0100

    fixed the get arbs endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner";
 import CardHolder from "./components/CardHolder/CardHolder";
@@ -9,19 +8,6 @@ import "./App.css";
 
 
 class App extends Component {
-
-  getAllArbs = () => {
-    
-    // axios.get("http://localhost:4000/api/v0/arbs")
-    //   .then(res => {
-    //     const games = res.data
-    //     this.setState({ games })
-    //   })
-    //   .catch(err => {
-    //     console.log("an error occurred in the componentDidMount API call", err)
-    //   })
-  }
-
 
   render() {
      return (
@@ -33,7 +19,7 @@ class App extends Component {
           <Footer />
         </div>
     );
-  }
+  } 
 
 }
 
--- frontend/src/components/MobiCard/MobiCardHolder.js
+++ frontend/src/components/MobiCard/MobiCardHolder.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import MobiCard from "./MobiCardLet/MobiCardLet";
 import "./mobicard.css";
-import { arbGames } from "../utils/arbs";
+// import { arbGames } from "../utils/arbs";
 
 class MobiCardHolder extends Component {
 
 	constructor() {
 		super()
 		this.state = {
-			games: []
+			games: [],
+			limit: 4
 		}
 	}
 
-	componentDidMount() {
-		this.setState({
-			games: arbGames
+	getArbsList = () => {
+		const { limit } = this.state
+		axios.get(`http://localhost:4000/api/v0/arbs?limit=${limit}`)
+			.then(res => {
+				if (res.status === 200) {
+					let games = res.data
+					this.setState({ games })
+				}
+			})
+			.catch(err => {
+				console.log("an error occurred in the componentDidMount API call in MobiCardHolder", err)
 		})
+	}
+
+	componentDidMount() {
+		this.getArbsList()
 	}
 
 	clickCard = (idx) => {
