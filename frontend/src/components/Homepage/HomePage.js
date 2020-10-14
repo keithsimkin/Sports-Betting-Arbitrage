5dcd166102c2c34092e74602f61d892580d1cb94
tunde <icepaigez@gmail.com>
2020-10-14 21:41:02 +0100

    created homepage component files

--- frontend/src/components/Homepage/HomePage.js
+++ frontend/src/components/Homepage/HomePage.js
@@ -8,11 +8,11 @@ const HomePage = () => {
 				<FaSnowflake className="arb_logo"/>
 			</div>
 			<div>
-				<p>Make money from sports, just like the stock market</p>
+				<p>Make money from sports, like the stock market</p>
 			</div>
 			<div>
 				<p>Join Playbook today.</p>
-				<div>
+				<div>	
 					<div>Sign up</div>
 					<div>Log in</div>
 				</div>
