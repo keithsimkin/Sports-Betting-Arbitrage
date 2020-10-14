e7437b2f69374279c5feac0101d1801eead2a91f
tunde <icepaigez@gmail.com>
2020-10-14 18:54:29 +0100

    created homepage jsx

---
+++ frontend/src/components/Homepage/HomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaSnowflake } from "react-icons/fa";
+
+const HomePage = () => {
+	return(
+		<div>
+			<div>
+				<FaSnowflake className="arb_logo"/>
+			</div>
+			<div>
+				<p>Make money from sports, just like the stock market</p>
+			</div>
+			<div>
+				<p>Join Playbook today.</p>
+				<div>
+					<div>Sign up</div>
+					<div>Log in</div>
+				</div>
+			</div>
+			<div>
+				image
+			</div>
+		</div>
+	)
+}
+
+export default HomePage;
