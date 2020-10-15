aba39c798091db68906febc9c936604df0784d23
tunde <icepaigez@gmail.com>
2020-10-15 08:32:22 +0100

    styling the home page

--- frontend/src/components/Routes/HomePage.js
+++ frontend/src/components/Routes/HomePage.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { FaSnowflake } from "react-icons/fa";
+import "./routes.css";
 
 const HomePage = ({ onRouteChange }) => {
 	return(
-		<div className="homepage-uppersection">
-			<div>
+		<div className="homepage-container">
+			<div className="homepage-uppersection">
 				<div>
-					<FaSnowflake className="arb_logo"/>
+					<FaSnowflake className="arb_logo_homepage"/>
+				</div>
+				<div className="homepage-text">
+					<p>Make money from sports like the stock market</p>
 				</div>
 				<div>
-					<p>Make money from sports, like the stock market</p>
-				</div>
-				<div>
-					<p>Join Mockbe today.</p>
-					<div>	
-						<div onClick={() => onRouteChange('signup')}>Sign up</div>
-						<div onClick={() => onRouteChange('signin')}>Log in</div>
+					<p className="homepage-smalltext">Join Mockbe today.</p>
+					<div className="homepage-buttons">	
+						<div className="homepage-signup-button" onClick={() => onRouteChange('signup')}>Sign up</div>
+						<div className="homepage-login-button" onClick={() => onRouteChange('signin')}>Log in</div>
 					</div>
 				</div>
 			</div>
