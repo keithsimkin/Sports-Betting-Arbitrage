b95889805023c25aa6be040b994f0659efe25478
tunde <icepaigez@gmail.com>
2020-10-15 09:40:12 +0100

    styling the signin page

--- frontend/src/components/Routes/SignIn.js
+++ frontend/src/components/Routes/SignIn.js
@@ -18,8 +18,8 @@ const SignIn = ({ onRouteChange }) => {
 			<div>
 				<div className="signin-login-button" onClick={() => onRouteChange('home')}>Log in</div>
 			</div>
-			<div>
-				<p>Forgot password?</p>
+			<div className="signin-footer">
+				<p>Forgot password?</p> 
 				<p onClick={() => onRouteChange('signup')}>Sign up for Mockbe</p>
 			</div>
 		</div>
