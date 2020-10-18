cd6ae44902511b8f168c63c98c4e3d58578e1cce
tunde <icepaigez@gmail.com>
2020-10-19 00:08:23 +0100

    getting signup input data

--- frontend/src/components/Routes/SignUp.js
+++ frontend/src/components/Routes/SignUp.js
@@ -1,27 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
 import { FaSnowflake } from "react-icons/fa";
 
-const SignUp = ({ onRouteChange }) => {
-	return(
-		<div className="signup-container">
-			<div>
-				<FaSnowflake className="arb_logo_routes"/>
+class SignUp extends Component {
+
+	constructor() {
+		super()
+		this.state = {
+			signUpEmail:"",
+			signUpFirstName:"",
+			signUpLastName:"",
+			signUpUsername:"",
+			signUpPassword:""
+		}
+	}
+
+	getSignupEmail = e => {
+		this.setState({
+			signUpEmail: e.target.value
+		})
+	}
+
+	getSignupFirstName = e => {
+		this.setState({
+			signUpFirstName: e.target.value
+		})
+	}
+
+	getSignupLastName = e => {
+		this.setState({
+			signUpLastName: e.target.value
+		})
+	}
+
+	getSignupUserName = e => {
+		this.setState({
+			signUpUsername: e.target.value
+		})
+	}
+
+	getSignupPassword = e => {
+		this.setState({
+			signUpPassword: e.target.value
+		})
+	}
+
+	onSubmitSignup = () => {
+		console.log(this.state)
+	}
+
+	render() {
+		const { onRouteChange } = this.props
+		return(
+			<div className="signup-container">
+				<div>
+					<FaSnowflake className="arb_logo_routes"/>
+				</div>
+				<div className="signup-text">
+					<p>Sign up to invest in sports like the stock market</p>
+				</div>
+				<div className="signup-input">
+					<input onChange={this.getSignupEmail} type="email" name="email" placeholder="Email"/>
+					<input onChange={this.getSignupFirstName} type="text" name="first-name" placeholder="First Name"/>
+					<input onChange={this.getSignupLastName} type="text" name="last-name" placeholder="Last Name"/>
+					<input onChange={this.getSignupUserName} type="text" name="username" placeholder="Username"/>
+					<input onChange={this.getSignupPassword} type="password" name="password" placeholder="Password"/>
+				</div>
+				<div>
+					<div className="signup-button" onClick={() => onRouteChange('autoarb')}>Sign up</div>
+				</div>
 			</div>
-			<div className="signup-text">
-				<p>Sign up to invest in sports like the stock market</p>
-			</div>
-			<div className="signup-input">
-				<input type="email" name="email" placeholder="Email"/>
-				<input type="text" name="first-name" placeholder="First Name"/>
-				<input type="text" name="last-name" placeholder="Last Name"/>
-				<input type="text" name="username" placeholder="Username"/>
-				<input type="password" name="password" placeholder="Password"/>
-			</div>
-			<div>
-				<div className="signup-button" onClick={() => onRouteChange('autoarb')}>Sign up</div>
-			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default SignUp;
