3fbaf61c00c1d5f089f1efff6d8c7d1004925f48
tunde <icepaigez@gmail.com>
2020-10-19 00:30:12 +0100

    sending frontend data to BE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,8 +19,8 @@ class App extends Component {
   constructor(){
     super() 
     this.state = {
-      // route: 'index',
-      route: 'signin',
+      route: 'index',
+      // route: 'signin',
       bookieselection:[]
     }
   }
