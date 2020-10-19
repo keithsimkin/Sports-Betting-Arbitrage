b4106fbef81e76f50585fac1392d45e0dc2098d7
tunde <icepaigez@gmail.com>
2020-10-19 18:31:29 +0100

    testing scraper v0

--- scrapers/bet9ja/server.js
+++ scrapers/bet9ja/server.js
@@ -8,8 +8,7 @@ const PORT = process.env.PORT || 4001;
 
 
 (async () => {
-	
-
+	await bet9jaScraper().then(console.log).catch(err => console.log('an error occurred in the bet9ja scraper', err))
 
 	server.get("/", (req, res)=> {
 		res.status(200).send('this is the bet9ja server endpoint')
