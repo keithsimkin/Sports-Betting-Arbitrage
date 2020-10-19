b1d62e3b68b601111b178851adb6bb9f93b46aa9
tunde <icepaigez@gmail.com>
2020-10-19 14:02:34 +0100

    fixing the db ops server

--- backend/server.js
+++ backend/server.js
@@ -6,25 +6,30 @@ require('dotenv').config();
 const { createDBTables }  = require("./controllers/v0/root/models/createTables");
 const { IndexRouter } = require("./controllers/v0/index.router");
 
-const server = express();
-const PORT = process.env.PORT || 4000;
-
-server.use(bodyParser.json());
-server.use('/api/v0/', IndexRouter);
-
-server.get('/', (req, res) => {
-	res.send('/api/v0')
-});
-
-server.post('/', (req, res) => {
-	res.send('/api/v0')
-})
-
-server.listen(PORT, () => {
-	console.log(`the arb server is listening on port ${PORT}`)
-});
 
 
+(async () => {
+	await createDBTables().then(console.log).catch(err => console.log('an error occurred when creating tables', err))
+
+	const server = express();
+	const PORT = process.env.PORT || 4000;
+
+	server.use(bodyParser.json());
+
+	server.use('/api/v0/', IndexRouter);
+
+	server.get('/', (req, res) => {
+		res.send('/api/v0')
+	});
+
+	server.post('/', (req, res) => {
+		res.send('/api/v0')
+	})
+
+	server.listen(PORT, () => {
+		console.log(`the arb server is listening on port ${PORT}`)
+	});
+})()
 
 
 
