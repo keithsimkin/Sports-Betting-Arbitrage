dcaf663a6c3b3e9738a9075ab49ed4b8c7e13fd6
tunde <icepaigez@gmail.com>
2020-10-20 19:48:48 +0100

    adding user endpoints

--- backend/controllers/v0/index.router.js
+++ backend/controllers/v0/index.router.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 const { ArbsRouter } = require("./arbs/routes/arbs.router");
 const { RootRouter } = require("./root/routes/root.router");
+const { AuthRouter } = require("./users/routes/auth.router");
 
 router.use('/', RootRouter);
-router.use('/arbs', ArbsRouter);
+router.use('/arbs', ArbsRouter); 
+router.use('/users', AuthRouter);
 
 
 router.get('/', (req, res) => { 
