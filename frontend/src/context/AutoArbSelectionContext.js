6bf1c10a72673d02d448609c622119424a66dcb0
tunde <icepaigez@gmail.com>
2020-10-23 09:35:09 +0100

    fixing context

--- frontend/src/context/AutoArbSelectionContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const SelectionContext = React.createContext(true)
-
-export const SelectionProvider = SelectionContext.Provider 
-export const SelectionConsumer = SelectionContext.Consumer 
-
-export default SelectionContext
