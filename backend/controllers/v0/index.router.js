ca6e4149ef4615a09688a2c3978b76fd83da8c02
tunde <icepaigez@gmail.com>
2020-10-27 22:38:59 +0100

    installed cookie parser

--- backend/controllers/v0/index.router.js
+++ backend/controllers/v0/index.router.js
@@ -1,12 +1,19 @@
  "use strict";
 
 const express = require("express");
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
+const cookieParser = require('cookie-parser');
+
 const router = express.Router();
 
 const { ArbsRouter } = require("./arbs/routes/arbs.router");
 const { RootRouter } = require("./root/routes/root.router");
 const { AuthRouter } = require("./users/routes/auth.router");
 const { BookieRouter } = require("./bookie_logins/routes/logins.router");
+
+router.use(cookieParser());
+router.use(csrfProtection);
 
 router.use('/', RootRouter);
 router.use('/arbs', ArbsRouter); 
--- backend/controllers/v0/root/routes/root.router.js
+++ backend/controllers/v0/root/routes/root.router.js
@@ -1,16 +1,12 @@
 "use strict";
-
 const express = require("express");
-const csrf = require('csurf');
-const csrfProtection = csrf({ cookie: true });
 const router = express.Router();
 //const db = require("../../../../knex");
-
-router.use(csrfProtection);
 
 
 router.get('/', (req, res) => {
 	res.send({ csrfToken: req.csrfToken() })
+	//res.status(200).send('homebase')
 }); 
 
 exports.RootRouter = router;   
