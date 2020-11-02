99ba164dccc6cb202b1b924cf1d9fd57e79382b5
tunde <icepaigez@gmail.com>
2020-11-02 12:21:24 +0100

    implementing naijabet login function

---
+++ scrapers/naijabet/server.js
@@ -0,0 +1,33 @@
+"use strict";
+const express = require("express");
+const { naijabetArber } = require("./naijabet_arber");
+
+const server = express();
+const PORT = process.env.PORT || 4002;
+
+(async () => {
+
+	let data = {
+	    stake: '421.1',
+	    book: 'naijabet',
+	    odd: 2.7,
+	    market: '1X2',
+	    sport: 'Soccer',
+	    country: 'South Africa ',
+	    league: 'National First Division',
+	    game: 'TS Sporting vs Moroka Swallows FC',
+	    marketType: 'away',
+	    username: 'naijabetluthor',
+	    password: 'naijabetluthor'
+	  }
+
+	let resp = naijabetArber(data)
+	//console.log(resp)
+	server.get('/', (req, res) => {
+		res.status(200).send('naijabet server')
+	})
+
+	server.listen(PORT, () => {
+		console.log(`the naijabet server is listening on port ${PORT}`)
+	})
+})()
