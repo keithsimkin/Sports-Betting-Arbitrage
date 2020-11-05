f07ae905954b01ca7ef065e7faf1d4da38ba0cb0
tunde <icepaigez@gmail.com>
2020-11-05 21:17:58 +0100

    fixed naijabet autologin & started bet9ja autologin functionality

--- scrapers/naijabet/server.js
+++ scrapers/naijabet/server.js
@@ -7,25 +7,6 @@ const PORT = process.env.PORT || 4002;
 
 (async () => {
 
-	let data = {
-	    stake: '550',
-	    book: 'naijabet',
-	    odd: 3.74,
-	    market: '1X2',
-	    sport: 'Soccer',
-	    country: 'Germany',
-	    league: 'Bundesliga',
-	    game: 'FC Augsburg vs Hertha BSC',
-	    marketType: 'draw',
-	    username: 'icepaigez',
-	    password: 'win!freD@j0ju'
-	    // username: 'test',
-	    // password: 'test'
-	  }
-
-	let resp = naijabetArber(data)
-	resp.then(console.log)
-
 	server.get('/', (req, res) => {
 		res.status(200).send('naijabet server')
 	})
